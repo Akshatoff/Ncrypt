c46b062120aab93545e25a502709cd480de072c5
Akshatoff <akshatascoder@gmail.com>
2024-10-15 19:58:47 +0530

    My bad vro

--- script/script.js
+++ script/script.js
@@ -4,17 +4,66 @@ import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/l
 
 const upload = document.getElementById("upload");
 const view = document.getElementById("view");
+const uploads = document.getElementById("uploadsection");
+const views = document.getElementById("viewsection")
+const dd = document.getElementById("dd");
+const image = document.getElementById("image");
+const model = document.getElementById("model")
+const poster = document.getElementById("poster");
+const da = document.getElementById("da");
+
+image.style.display = "none";
+model.style.display = "none";
+poster.style.display = "none";
+da.style.display = "none";
+
+
+dd.addEventListener("change", function () {
+    if (dd.value == 1) {
+        image.style.display = "block";
+        model.style.display = "none";
+        poster.style.display = "none";
+        da.style.display = "none";
+    }
+    if (dd.value == 2) {
+        model.style.display = "block";
+        image.style.display = "none";
+        poster.style.display = "none";
+        da.style.display = "none";
+    }
+    if (dd.value == 3) {
+        poster.style.display = "block";
+        image.style.display = "none";
+        model.style.display = "none";
+        da.style.display = "none";
+    }
+    if (dd.value == 4) {
+        da.style.display = "block";
+        image.style.display = "none";
+        model.style.display = "none";
+        poster.style.display = "none";
+    }
+})
+
+
+
+
+
 
 upload.classList.add("active");
 
 upload.addEventListener("click", function () {
     upload.classList.add("active");
     view.classList.remove("active");
+    views.style.display = "none";
+    uploads.style.display = "flex";
 })
 
 view.addEventListener("click", function () {
     upload.classList.remove("active");
     view.classList.add("active")
+    uploads.style.display = "none";
+    views.style.display = "block";
 })
 
 const signUpButton = document.getElementById('signUp');
@@ -24,17 +73,17 @@ const transbtn = document.getElementById("tran");
 
 
 signUpButton.addEventListener('click', () => {
-	containerf.classList.add("right-panel-active");
+    containerf.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener('click', () => {
-	containerf.classList.remove("right-panel-active");
-}); 
+    containerf.classList.remove("right-panel-active");
+});
 
-transbtn.addEventListener("click", function() {
+transbtn.addEventListener("click", function () {
     containerf.classList.add("right-panel-active");
-  
-    
+
+
 })
 
 
