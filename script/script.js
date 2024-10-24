ae16c6426a14e41e6d8833adaf0c966c8d1d1731
Akshatoff <akshatascoder@gmail.com>
2024-10-24 16:57:30 +0530

    Update

--- script/script.js
+++ script/script.js
@@ -151,9 +151,9 @@ var x = setInterval(function () {
     var minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((time % (1000 * 60)) / 1000);
 
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("hours").innerHTML = hours;
-    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("days").innerHTML = days + " : ";
+    document.getElementById("hours").innerHTML = hours + " : ";
+    document.getElementById("minutes").innerHTML = minutes + " : ";
     document.getElementById("seconds").innerHTML = seconds;
 
 }, 1000)
